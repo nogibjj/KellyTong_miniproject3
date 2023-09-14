a20d5c19f8d857a2d60272884dd4afc78493b446
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-14 14:14:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-def subtraction(x,y):
-    result = 0
-    result = x - y
-    return result
+import matplotlib.pyplot as plt
+import seaborn as sns
+import polars as pl
+
+def age():
+    df=pl.read_csv("gss.csv")
+    print(df.shape)
+    print(df.describe())
+    plot = sns.histplot(df["age"], kde=True, color="blue", label="Age")
+    plot.legend()
+    plt.show()
+    
+def desrcibe_dataframe():
+    data = {'height': [170,175,160,180,190]
+    }
+    dataframe = pd.DataFrame(data)
+    return dataframe.describe()
