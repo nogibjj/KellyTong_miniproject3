15e20471211cd5f6954e925b8ed1d779528303bf
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-15 15:01:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import polars as pl
 
-def summary():
-    df=pl.read_csv("gss.csv")
-    df.dropna()
-    print(df.median())
-    print(df.describe())
+#def summary():
+  #  df=pl.read_csv("gss.csv")
+   # df.dropna()
+   # print(df.median())
+   # print(df.describe())
     
 def age():
     df=pl.read_csv("gss.csv")
+    print(df.median())
+    print(df.describe())
     plot = sns.histplot(df["age"], kde=True, color="blue", label="Age")
     plot.legend()
     plt.show()
