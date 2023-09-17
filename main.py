db12d0ae64f2799fcffc5aa934adda0ebeac22b1
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-17 17:24:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-
 import polars as pl
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 #def summary():
   #  df=pl.read_csv("gss.csv")
@@ -9,12 +10,11 @@ import polars as pl
     
 def age():
     df = pl.read_csv("gss2.csv", infer_schema_length=10000)
-    
-    #print(df.median())
+    print(df.median())
     print(df.describe())
-    #plot = sns.histplot(df["age"], kde=True, color="blue", label="Age")
-    #plot.legend()
-    #plt.show()
+    plot = sns.histplot(df["age"], kde=True, color="blue", label="Age")
+    plot.legend()
+    plt.show()
     
 #def desrcibe_dataframe():
   #  data = {'height': [170,175,160,180,190]
