72827e7a32e232cd6c757f4dcd2a49a4c0264f24
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-17 17:52:12 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 #this is for testing functions in main.py
 
-from main import summary_pandas, summary_polars, age_pandas, age_polars, desrcibe_dataframe
+from main import summary_pandas, summary_polars, age_pandas, age_polars
 
 def test_summary_pd():
     summary_pandas()
@@ -13,11 +13,3 @@ def test_age_pd():
 
 def test_age_pl():
     age_polars()
-
-def test_describe():
-    "testing the desrcibe_dataframe function in main.py"
-    describe = desrcibe_dataframe()
-    assert describe['count'][0]== 5.0
-    assert describe['mean'][0] == 175.0
-    assert describe['min'][0] == 160.0
-    assert describe['max'][0] == 190.0
